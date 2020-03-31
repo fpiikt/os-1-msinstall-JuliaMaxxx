cbd4a565179710049852b5e49e07341640f5a25f
JuliaMaxxx <62603984+JuliaMaxxx@users.noreply.github.com>
2020-03-31 16:26:16 +0300

    In progress

---
+++ jre-8.bat
@@ -0,0 +1,4 @@
+#Установка Java Runtime Environment + скрипт, позволяющий установить JRE 8 с использованием
+#конфигурационного файла, содержащего следующие параметры
+
+C:\>jre-8u241-windows-x64.exe INSTALLCFG="%cd%jre-8_configuration.txt" /L "%cd%jre-log.txt"
